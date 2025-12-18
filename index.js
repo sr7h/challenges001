6f3d9897d156d8275e2f54b17dabd670ba102c17
Shaheer <149281026+sr7h@users.noreply.github.com>
2025-12-18 21:47:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const data = [
+    "ZmxhZ3t0aGlua18=",   // part A
+    "dGhlX2JveH0=",       // part C
+    "ZmFrZV9wYXJ0"        // fake
+];
+
+function check() {
+    const user = document.getElementById("flagInput").value;
+
+    if (user === buildFlag()) {
+        document.getElementById("msg").innerText = "✅ Correct!";
+        document.getElementById("msg").style.color = "green";
+    } else {
+        document.getElementById("msg").innerText = "❌ Try again";
+        document.getElementById("msg").style.color = "red";
+    }
+}
+
+function buildFlag() {
+    // الجزء الناقص ليس هنا 😉
+    return atob(data[0]) + atob(data[1]);
+}
