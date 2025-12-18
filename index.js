dbf2b88d897248337144ced9eb556919855f3f00
Shaheer <149281026+sr7h@users.noreply.github.com>
2025-12-18 22:36:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const data = [
+بconst data = [
     "ZmxhZ3t0aGlua18=",   // part A
     "dGhlX2JveH0=",       // part C
     "ZmFrZV9wYXJ0"        // fake
@@ -20,3 +20,5 @@ function buildFlag() {
     // الجزء الناقص ليس هنا 😉
     return atob(data[0]) + atob(data[1]);
 }
+
+// ZmxhZ3t0aGlua18=
